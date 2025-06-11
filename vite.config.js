0535213add0db1cb0efbbf683031557967eb21ba
abok-cymk <allanabok18@gmail.com>
2025-06-11 16:56:05 +0300

    first commit

---
+++ vite.config.js
@@ -0,0 +1,12 @@
+import { defineConfig } from "vite";
+
+export default defineConfig({
+    root: "./",
+    build: {
+        outDir: "dist",
+    },
+    server: {
+        port: 3000,
+    },
+    base: "/testimonials-grid-section-main/"
+})
